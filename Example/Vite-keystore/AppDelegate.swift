a339e92eb0b56e0b130bc5b871ef4bd85dbce930
曹亮 <caoliang02@meituan.com>
2018-09-04 17:09:56 +0800

    初始化

--- Example/Vite-keystore/AppDelegate.swift
+++ Example/Vite-keystore/AppDelegate.swift
@@ -5,8 +5,6 @@
 //  Created by Water on 2018/8/29.
 //  Copyright © 2018年 Water. All rights reserved.
 //
-
-
 import UIKit
 import Vite_keystore
 
@@ -16,17 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let numbers = [1, 2, 6, 4, 50,1]
-        print(numbers.suffix(2))
-
-        let str = Mnemonic.randomGenerator(strength: .strong, language: .english)
-
-        let entropy = Data(hex: "000102030405060708090a0b0c0d0e0f")
-        let mnemonic = Mnemonic.generator(entropy: entropy)
-        let mnemonic_zh = Mnemonic.generator(entropy: entropy,language:.simplifiedChinese)
-        let mnemonic_zh1 = Mnemonic.generator(entropy: entropy,language:.traditionalChinese)
-
-
         let rootVC = ViewController()
         window = UIWindow.init(frame:UIScreen.main.bounds)
         let nav = UINavigationController.init(rootViewController: rootVC)
--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # Vite-keystore
 
-[![CI Status](https://img.shields.io/travis/曹亮/Vite-keystore.svg?style=flat)](https://travis-ci.org/曹亮/Vite-keystore)
-[![Version](https://img.shields.io/cocoapods/v/Vite-keystore.svg?style=flat)](https://cocoapods.org/pods/Vite-keystore)
-[![License](https://img.shields.io/cocoapods/l/Vite-keystore.svg?style=flat)](https://cocoapods.org/pods/Vite-keystore)
-[![Platform](https://img.shields.io/cocoapods/p/Vite-keystore.svg?style=flat)](https://cocoapods.org/pods/Vite-keystore)
-
 ## Example
 
 To run the example project, clone the repo, and run `pod install` from the Example directory first.
@@ -22,7 +17,7 @@ pod 'Vite-keystore'
 
 ## Author
 
-曹亮, caoliang02@meituan.com
+Water, caoliang@vite.org
 
 ## License
 
