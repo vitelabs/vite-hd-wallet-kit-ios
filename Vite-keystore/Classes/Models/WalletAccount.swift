83e9018db962f3bb04ff25ec39ae77333814592f
Stone <shitong@vite.org>
2018-09-17 14:57:08 +0800

    add uuid

--- Vite-keystore/Classes/Models/WalletAccount.swift
+++ Vite-keystore/Classes/Models/WalletAccount.swift
@@ -11,6 +11,7 @@ import ObjectMapper
 
 public class WalletAccount : Mappable{
 
+    public var uuid = UUID().uuidString
     public var mnemonic = ""
     public var password = ""
     public var name = ""
@@ -20,12 +21,6 @@ public class WalletAccount : Mappable{
 
     //account is or not login, in order to use it
     public var isLogin = false
-    public var addressCount = 1 {
-        didSet {
-            existKeys = generateExistKeys()
-        }
-    }
-    public var defaultAddressIndex = 0
 
     public init() {
 
@@ -36,53 +31,11 @@ public class WalletAccount : Mappable{
     }
 
     public func mapping(map: Map) {
+        uuid    <- map["uuid"]
         mnemonic    <- map["mnemonic"]
         password    <- map["password"]
         name    <- map["name"]
         isSwitchTouchId    <- map["isSwitchTouchId"]
         isLogin    <- map["isLogin"]
-        addressCount    <- map["addressCount"]
-        defaultAddressIndex    <- map["defaultAddressIndex"]
-    }
-
-    public lazy var existKeys: [Key] = self.generateExistKeys()
-    public var defaultKey: Key { return existKeys[defaultAddressIndex] }
-}
-
-extension WalletAccount {
-    public struct Key {
-        public var secretKey: String
-        public var publicKey: String
-        public var address: String
-    }
-
-    public var canGenerateAddress: Bool {
-        return addressCount < 10
-    }
-
-    public func generateAddress() {
-        if canGenerateAddress {
-            addressCount += 1
-            // TODO: need to save
-        }
-    }
-
-    public func setDefaultAddressIndex(_ index: Int) {
-        if index < addressCount {
-            defaultAddressIndex = index
-        }
-    }
-
-    fileprivate func generateExistKeys() -> [Key] {
-        let seed = Mnemonic.createBIP39Seed(mnemonic: mnemonic).toHexString()
-        let keys = NSMutableArray()
-        for index in 0..<addressCount {
-            let path = "\(HDBip.viteAccountPrefix)/\(index-1)'"
-            if let (secretKey, publicKey, address) = HDBip.accountsForIndex(index, seed: seed) {
-                let key = Key(secretKey: secretKey, publicKey: publicKey, address: address)
-                keys.add(key)
-            }
-        }
-        return keys as! Array<Key>
     }
 }
