1842af8952dd999b2431f50f9c53214b2c49d195
water <daima.cn@gmail.com>
2019-02-26 12:42:50 +0800

    upgrade pod

--- Vite-keystore/Classes/Ethereum/Ethereum.swift
+++ Vite-keystore/Classes/Ethereum/Ethereum.swift
@@ -5,9 +5,9 @@
 //  Created by Water on 2018/9/21.
 //
 
-import Foundation
-
-public enum Ethereum {
-    public static let privateKeySize = 32
-    public static let addressSize = 20
-}
+//import Foundation
+//
+//public enum Ethereum {
+//    public static let privateKeySize = 32
+//    public static let addressSize = 20
+//}
